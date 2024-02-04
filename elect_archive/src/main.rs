d8ff2c171c9a7acb8a1d93e58b9855efd978ff79
watcher041 <watcher041@gmail.com>
2024-02-04 10:03:23 +0900

    ホーム画面を表示

--- elect_archive/src/main.rs
+++ elect_archive/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+
+// 必要なクレートを定義
+use actix_web::{web, App, HttpServer, Responder};
+
+// 非同期関数を定義
+async fn home() -> impl Responder {
+    "Hello,world!"
+}
+
+// メイン関数
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| 
+        App::new()
+        .route("/", web::get().to(home)))
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
 }
