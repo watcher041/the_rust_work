f75480bfcb1dfee5c83bbee4c55a44c7c95a8372
watcher041 <watcher041@gmail.com>
2024-02-04 13:07:25 +0900

    main関数を変更

--- elect_archive/src/main.rs
+++ elect_archive/src/main.rs
@@ -7,8 +7,7 @@ use actix_web::{App, HttpServer};
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| 
-        App::new()
-        .configure(routes::routing) 
+        App::new().configure(routes::routing) 
     )
     .bind("localhost:3000")?
     .run()
